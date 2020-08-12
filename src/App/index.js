1690813e0341870ba10d9174972204e257ab8935
lalebdi <loai.alebdi@gmail.com>
2020-08-12 19:36:17 -0400

    added loading content

--- src/App/index.js
+++ src/App/index.js
@@ -4,14 +4,16 @@ import AppBar from './AppBar';
 import AppLayout from './AppLayout';
 import {AppProvider} from './AppProvider';
 import Settings from '../Settings/Index';
-
+import Content from '../Shared/Content'
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
       <AppBar />
+      <Content>
       <Settings />
+      </Content>
     </AppProvider>
     </AppLayout>
   );
