6bfdf2789fb876e790595511b5a3ccdfaeaaf63c
lalebdi <loai.alebdi@gmail.com>
2020-08-12 20:07:49 -0400

    Created a coin grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { SelectableTile } from '../Shared/Tile';
 
 export const CoinGridStyled = styled.div`
 display : grid;
 grid-template-columns: repeat(5, 1fr);
+grid-gap: 15px;
 `
 // the above will divide them into 5 columns stretched
-
+// the grid-gap will put a gap between the rows and columns but not on the edges
 export default function CoinGrid() {
     return (
         <AppContext.Consumer>
             {({coinList}) => <CoinGridStyled>
                 {Object.keys(coinList).map(coinKey => 
-                    <div> {coinKey} </div>)}
+                    <SelectableTile> {coinKey} </SelectableTile>)}
             </CoinGridStyled> }
         </AppContext.Consumer>
     )
