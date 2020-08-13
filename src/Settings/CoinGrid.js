57bbe38f5ee00a3ec8a321930745b8fe180e99b6
lalebdi <loai.alebdi@gmail.com>
2020-08-12 21:27:53 -0400

    Added top section component

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -12,16 +12,17 @@ margin-top: 40px;
 // the above will divide them into 5 columns stretched
 // the grid-gap will put a gap between the rows and columns but not on the edges
 
-function getCoinToDisplay(coinList){
-    return Object.keys(coinList).slice(0, 100);
+function getCoinToDisplay(coinList, topSection){
+    // Wanted to display 10 in the top section 
+    return Object.keys(coinList).slice(0, topSection? 10 : 100);
 }
 
-export default function CoinGrid() {
+export default function CoinGrid({topSection}) {
     return (
         <AppContext.Consumer>
             {({coinList}) => <CoinGridStyled>
-                {getCoinToDisplay(coinList).map(coinKey => 
-                    <CoinTile coinKey={coinKey} />)}
+                {getCoinToDisplay(coinList, topSection).map(coinKey => 
+                    <CoinTile coinKey={coinKey} topSection={topSection} />)}
             </CoinGridStyled> }
         </AppContext.Consumer>
     )
--- src/Settings/Index.js
+++ src/Settings/Index.js
@@ -8,6 +8,7 @@ export default function Index() {
     return (
         <Page name="settings">
             <WelcomeMessage />
+            <CoinGrid topSection/>
             <ConfirmButton />
             <CoinGrid />
         </Page>
